266f3f2af5326461d5ab28d0961d028fe6240dcf
danishaz <danishaz112345@gmail.com>
2022-10-30 01:00:48 +0300

    [WIP] add subject view

---
+++ app/src/main/java/com/kpfu/kfutimetable/commonwidgets/SubjectView.kt
@@ -0,0 +1,13 @@
+package com.kpfu.kfutimetable.commonwidgets
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.cardview.widget.CardView
+
+class SubjectView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : CardView(context, attrs, defStyleAttr) {
+
+}
