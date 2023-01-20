cb28cb03283ba7d161a98984aeed957658919e32
Kathy_Laip <ekaterinaslapnikova@Ekaterinas-MacBook-Air.local>
2023-01-20 17:21:36 +0300

    PopravkiToVerstka

--- app/src/main/java/com/kpfu/kfutimetable/presentation/mainscreen/CalendarFragment.kt
+++ app/src/main/java/com/kpfu/kfutimetable/presentation/mainscreen/CalendarFragment.kt
@@ -52,7 +52,7 @@ class CalendarFragment @Inject constructor(
 
     override fun render(currentViewState: CalendarViewState) = with(binding) {
         dayItemCarousel.render(currentViewState.dayItemCarouselState)
-        timetableView.render(Any())
+        timetableView.render(listOf())
     }
 
     private fun setListeners() = with(binding) {
